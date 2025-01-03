02c0e6af775826cb0308f7b49e8c218e7ddb0975
yazanbanikhalaf <yazan@josequal.com>
2025-01-04 00:08:03 +0300

    n

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,14 +2,28 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Book extends Model
 {
-    use HasFactory; 
+    use HasFactory;
+    protected $guarded;
     public function reviews()
     {
         return $this->hasMany(Review::class, 'book_id');
+    }
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+        return $query->where('title', 'LIKE', '%'. $title .'%');
+    }
+    public function scopePopular(Builder $query): Builder
+    {
+        return $query->withCount('reviews')->orderBy('reviews_count', 'desc');
+    }
+    public function scopeHighestRated(Builder $query): Builder
+    {
+        return $query->withAvg('reviews','rating')->orderBy('reviews_avg_rating', 'desc');
     }
 }
